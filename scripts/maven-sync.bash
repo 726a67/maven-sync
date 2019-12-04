27b369983e9bc540ab6b8476a3be91b820eb12f9
root <root@artifactory7.goldberg.af.mil>
2019-12-04 13:20:01 -0500

    separate projects

--- scripts/maven-sync.bash
+++ scripts/maven-sync.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MAVEN_PROJECTS_DIR=/root/airbridge/maven/projects
+MAVEN_PROJECTS_DIR=~/maven-sync/projects
 
 for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn -U clean dependency:go-offline package; echo -e; done
 
