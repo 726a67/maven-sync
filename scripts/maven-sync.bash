ad627cf5b2e431d71f241f41696e6078c6d99bd1
root <root@artifactory7.dadt.net>
2020-07-31 09:50:14 -0400

    refactor to use go-offline-maven-plugin

--- scripts/maven-sync.bash
+++ scripts/maven-sync.bash
@@ -3,6 +3,7 @@
 MAVEN_PROJECTS_DIR=~/maven-sync/projects
 
 #for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn -U clean dependency:go-offline package; echo -e; done
-for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn -U clean dependency:go-offline validate compile package install; mvn -U clean; echo -e; done
+#for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn -U clean dependency:go-offline validate compile package install; mvn -U clean; echo -e; done
+for PROJECT in `ls ${MAVEN_PROJECTS_DIR}` ; do echo "[${PROJECT}]"; echo -e ; cd ${MAVEN_PROJECTS_DIR}/${PROJECT}; mvn de.qaware.maven:go-offline-maven-plugin:resolve-dependencies ; echo -e; done
 
 cd ~
